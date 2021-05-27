e075348815d285c1a06100743590a83a14baf43f
théodore <theodore@MacBook-Air-de-theodore.local>
2021-05-27 12:12:42 +0200

    Added a new function

--- src/main/java/com/ipiecoles/Main.java
+++ src/main/java/com/ipiecoles/Main.java
@@ -1,15 +1,18 @@
 package com.ipiecoles;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import com.google.gson.Gson;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         EphemerideService ephemerideService = new EphemerideService();
         //System.out.println(ephemerideService.getResponse());
-        System.out.println(new Gson().toJson(ephemerideService.getResponse(LocalDate.now())));
-        System.out.println(new Handler().handleRequest(null, null));
+        //System.out.println(new Gson().toJson(ephemerideService.getResponse(LocalDate.now())));
+        //System.out.println(new Handler().handleRequest(null, null));
+        Handler handler = new Handler();
+        GatewayRequest params = new GatewayRequest();
+        params.setBody("{\"name\":[[\"theodore\"]]}");
+        //params.setBody("");
+        System.out.println(handler.handleRequestWithParameter(params, null));
     }
 }
